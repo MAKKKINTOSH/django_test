f6524f80c09e2bda0f4db71479a6dad69d897a2f
Ivan <us.ivan05@gmail.com>
2023-06-06 17:22:36 +0800

    its just work its just work

--- test_site/polls/views.py
+++ test_site/polls/views.py
@@ -1,28 +1,30 @@
 from django.http import HttpResponse, HttpRequest
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question, Choice
 
 
 def index(request: HttpRequest):
     nearest_questions = Question.objects.order_by("-date_of_publication")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "nearest_questions": nearest_questions,
     }
-    return (HttpResponse(template.render(context, request)))
+    return render(request, "polls/index.html", context)
 
 
 def detail(request: HttpRequest, question_id):
-    response = "There is a next question: {} "
-    return HttpResponse(response.format(question_id))
+    question = Question.objects.filter(id=question_id)[0]
+    context ={"question": question}
+    return HttpResponse(render(request, "polls/detail.html", context))
 
 
 def results(request: HttpRequest, question_id):
-    response = "There is a result of next question: {} "
-    return HttpResponse(response.format(question_id))
+    question = Question.objects.filter(id=question_id)[0]
+    context = {"question": question}
+    return HttpResponse(render(request, "polls/results.html", context))
 
 
 def vote(request: HttpRequest, question_id):
-    response = "You are voting on question: {} "
-    return HttpResponse(response.format(question_id))
+    question = Question.objects.filter(id=question_id)[0]
+    context = {"question": question}
+    return HttpResponse(render(request, "polls/vote.html", context))
