b69627267b704dd4a0434d703f4ec75867fd6224
Ivan <us.ivan05@gmail.com>
2023-06-06 21:48:52 +0800

    the application works without troubles

--- test_site/polls/views.py
+++ test_site/polls/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpRequest, Http404
+from django.http import HttpRequest, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 
-from .models import Question
+from .models import Question, Choice
 
 
 def index(request: HttpRequest):
@@ -19,12 +20,29 @@ def detail(request: HttpRequest, question_id):
 
 
 def results(request: HttpRequest, question_id):
-    question = Question.objects.get(id=question_id)
+    question = Question.objects.get(pk=question_id)
     context = {"question": question}
     return render(request, "polls/results.html", context)
 
 
 def vote(request: HttpRequest, question_id):
+
     question = Question.objects.get(id=question_id)
-    context = {"question": question}
-    return render(request, "polls/vote.html", context)
+    context = {
+        "question": question,
+        "error_message": "You didn't select a choice!"
+    }
+
+    try:
+        print(request.POST, request.GET, request.META, sep="\n")
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except(KeyError, Choice.DoesNotExist):
+        return render(
+            request,
+            "polls/detail.html",
+            context
+        )
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
